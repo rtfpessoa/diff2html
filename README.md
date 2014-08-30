0b807ce01b7e25a7c911d7de9331a04e96747ce3
Rodrigo Fernandes <rtfrodrigo@gmail.com>
2014-08-30 03:54:52 +0100

    initial lines

---
+++ README.md
@@ -0,0 +1,38 @@
+# Diff to Html by [rtfpessoa](https://github.com/rtfpessoa)
+
+Diff to Html generates pretty HTML diffs from regular command line tools output.
+
+### Inspiration
+
+This project is inspired in [pretty-diff](https://github.com/scottgonzalez/pretty-diff) by [Scott González](https://github.com/scottgonzalez).
+This project started with some code from `pretty-diff` but it was then re-written and is now a
+browser library.
+
+## Features
+
+* line-by-line diff (not side-by-side)
+
+* new and old line numbers
+
+* GitHub like style
+
+## Setup
+
+> Simply import the diff2html.js invoke:
+
+    var diff2Html = Diff2Html.getInstance();
+    diff2Html.generatePrettyDiff(exInput);
+
+> Check out the `template.html` for a complete example.
+
+## Contribution
+
+All the contributions are welcome.
+
+To contribute just send a pull request with your feature,fix,... and it will be reviewed asap.
+
+## License
+
+Copyright 2014 Rodrigo Fernandes. Released under the terms of the MIT license.
+
+---
--- diff2html.js
+++ diff2html.js
@@ -1,188 +1,210 @@
 /*
  *
- * Diff(git) to HTML
+ * Diff to HTML (diff2html.js)
  * Author: rtfpessoa
  * Date: Friday 29 August 2014
  *
  */
 
-var exInput = 'diff --git a/conf/load-analysis.conf b/conf/load-analysis.conf\n' +
-'new file mode 100644\n' +
-'index 0000000..b170d2b\n' +
-'--- /dev/null\n' +
-'+++ b/conf/load-analysis.conf\n' +
-'@@ -0,0 +1,5 @@\n' +
-'+include "load-master-analysis.conf"\n' +
-'+\n' +
-'+\n' +
-'+# if should manage tasks\n' +
-'+codacy.isTaskManager=false\n' +
-'diff --git a/conf/load-master-analysis.conf b/conf/load-master-analysis.conf\n' +
-'new file mode 100644\n' +
-'index 0000000..53e049f\n' +
-'--- /dev/null\n' +
-'+++ b/conf/load-master-analysis.conf\n' +
-'@@ -0,0 +1,26 @@\n' +
-'+include "load.conf"\n' +
-'+\n' +
-'+# Sockets\n' +
-'+# ~~~~~\n' +
-'+codacy.sockets.remote=true\n' +
-'+codacy.sockets.url="http://load.codacy.com/socket/notify"\n' +
-'+\n' +
-'+# enables the analysis server to run\n' +
-'+codacy.isAnalysisServer=true\n' +
-'+# if should manage analysis server work\n' +
-'+codacy.isServerManager.active=false\n' +
-'+# if should manage tasks\n' +
-'+codacy.isTaskManager=true\n' +
-'+\n' +
-'+\n' +
-'+# enables the repositoryListener to run\n' +
-'+codacy.isRepositoryListener=true\n' +
-'+#enables the payment system pooling for stripe payments\n' +
-'+codacy.payments.active=false\n' +
-'+\n' +
-'+#third party\n' +
-'+codacy.thirdPartyNotification.akka.active=true\n' +
-'+codacy.thirdPartyNotification.users.active=false\n' +
-'+\n' +
-'+# DB values\n' +
-'+db.default.hikaricp.file="conf/database/hikaricp.production-server.properties"\n' +
-'diff --git a/conf/load-website.conf b/conf/load-website.conf\n' +
-'new file mode 100644\n' +
-'index 0000000..aa43adb\n' +
-'--- /dev/null\n' +
-'+++ b/conf/load-website.conf\n' +
-'@@ -0,0 +1,26 @@\n' +
-'+include "load.conf"\n' +
-'+\n' +
-'+# Sockets\n' +
-'+# ~~~~~\n' +
-'+codacy.sockets.remote=false\n' +
-'+codacy.sockets.url=""\n' +
-'+\n' +
-'+# enables the analysis server to run\n' +
-'+codacy.isAnalysisServer=false\n' +
-'+# if should manage analysis server work\n' +
-'+codacy.isServerManager.active=true\n' +
-'+# if should manage tasks\n' +
-'+codacy.isTaskManager=false\n' +
-'+\n' +
-'+\n' +
-'+# enables the repositoryListener to run\n' +
-'+codacy.isRepositoryListener=false\n' +
-'+#enables the payment system pooling for stripe payments\n' +
-'+codacy.payments.active=true\n' +
-'+\n' +
-'+#third party\n' +
-'+codacy.thirdPartyNotification.akka.active=false\n' +
-'+codacy.thirdPartyNotification.users.active=true\n' +
-'+\n' +
-'+#DB values\n' +
-'+db.default.hikaricp.file="conf/database/hikaricp.production-website.properties"\n' +
-'diff --git a/conf/load.conf b/conf/load.conf\n' +
-'new file mode 100644\n' +
-'index 0000000..8463b41\n' +
-'--- /dev/null\n' +
-'+++ b/conf/load.conf\n' +
-'@@ -0,0 +1,27 @@\n' +
-'+include "application.conf"\n' +
-'+include "auth/integration.conf"\n' +
-'+include "payments/test.conf"\n' +
-'+include "actors/production.conf"\n' +
-'+\n' +
-'+# Application configuration\n' +
-'+# ~~~~~~~~~~~~~~~~~~~~~~~~~\n' +
-'+application.mode=prod\n' +
-'+\n' +
-'+#Prevents integration from sending emails. DO NOT CHANGE\n' +
-'+mail.debug=true\n' +
-'+\n' +
-'+# Codacy\n' +
-'+# ~~~~~\n' +
-'+codacy.js.extension=".js"\n' +
-'+codacy.keys.location="/data/codacy/keys/"\n' +
-'+codacy.repository.location="/data/codacy/repos/"\n' +
-'+codacy.secure=false\n' +
-'+codacy.algorithms.location="public/javascripts/_engine/_algos/"\n' +
-'+\n' +
-'+codacy.url="http://load.codacy.com"\n' +
-'+\n' +
-'+db.default.maxActive=20\n' +
-'+\n' +
-'+db.default.url="jdbc:postgresql://loaddb.codacy.com:5432/codacy"\n' +
-'+db.default.user="codacy"\n' +
-'+db.default.password="codacy"';
+(function($, window) {
+  var ClassVariable;
 
-function generatePrettyDiff( diffInput ) {
-  var diffHtml = "";
-  var diffFiles = splitByFile( diffInput );
+  ClassVariable = (function() {
 
-  for ( var file in diffFiles ) {
-    diffHtml += "<h2>" + file + "</h2>" +
-                "<div class=\"file-diff\">" +
-                  "<div>" +
-                    markUpDiff( diffFiles[ file ] ) +
-                  "</div>" +
-                "</div>";
-  }
+    var CSS_STYLES = {
+      INFO: "info",
+      CONTEXT: "context",
+      NEW: "insert",
+      DELETED: "delete"
+    };
 
-  return diffHtml;
-}
+    var BLOCK_HEADER_LINE = "..."
 
-function splitByFile( diffInput ) {
-	var filename,
-		isEmpty = true;
-		files = {};
+      function Diff2Html() {}
 
-	diffInput.split( "\n" ).forEach(function( line, i ) {
-		// Unmerged paths, and possibly other non-diffable files
-		// https://github.com/scottgonzalez/pretty-diff/issues/11
-		if ( !line || line.charAt( 0 ) === "*") {
-			return;
-		}
+    Diff2Html.prototype.generatePrettyDiff = function(diffInput) {
+      var diffFiles = splitByFile(diffInput);
+      var html = generateHtml(diffFiles);
 
-		if ( line.charAt( 0 ) === "d" ) {
-			isEmpty = false;
-			filename = line.replace( /^diff --git a\/(\S+) b\/(\S+).*$/, "$2" );
-			files[ filename ] = [];
-		} else if ( line.indexOf( "diff --git" ) === 0 ||
-                line.indexOf( "new file mode" ) === 0 ||
-                line.indexOf( "index" ) === 0 ||
-                line.indexOf( "---" ) === 0 ||
-                line.indexOf( "+++" ) === 0) {
-      return;
-    } else {
-      files[ filename ].push( line );
-    }
-	});
+      return html;
+    };
 
-	return isEmpty ? null : files;
-}
+    var splitByFile = function(diffInput) {
+      var files = [],
+        currentFile = null,
+        currentBlock = null,
+        oldLine = null,
+        newLine = null;
 
-var markUpDiff = function () {
-	var diffClasses = {
-		"d": "file",
-		"i": "file",
-		"@": "info",
-		"-": "delete",
-		"+": "insert",
-		" ": "context"
-	};
+      diffInput.split("\n").forEach(function(line) {
+        // Unmerged paths, and possibly other non-diffable files
+        // https://github.com/scottgonzalez/pretty-diff/issues/11
+        // Also, remove some useless lines
+        if (!line || line.charAt(0) === "*" ||
+          line.indexOf("new") === 0 ||
+          line.indexOf("index") === 0 ||
+          line.indexOf("---") === 0 ||
+          line.indexOf("+++") === 0) {
+          return;
+        }
 
-	function escape( str ) {
-		return str
-			.replace( /&/g, "&amp;" )
-			.replace( /</g, "&lt;" )
-			.replace( />/g, "&gt;" )
-			.replace( /\t/g, "    " );
-	}
+        if (line.indexOf("diff") === 0) {
+          /* File Diff Line */
 
-	return function( diff ) {
-		return diff.map(function( line ) {
-			var type = line.charAt( 0 );
-			return "<pre class=\"" + diffClasses[ type ] + "\">" + escape( line ) + "</pre>";
-		}).join( "\n" );
-	};
-}();
+          /* add previous block(if exists) before start a new file */
+          if (currentBlock) {
+            currentFile.blocks.push(currentBlock);
+            currentBlock = null;
+          }
+
+          /* add previous file(if exists) before start a new one */
+          if (currentFile) {
+            files.push(currentFile);
+            currentFile = null;
+          }
+
+          /* create file structure */
+          currentFile = {};
+          currentFile.blocks = [];
+
+          /* save file paths, before and after the diff */
+          var values = /^diff --git a\/(\S+) b\/(\S+).*$/.exec(line);
+          currentFile.oldName = values[1];
+          currentFile.newName = values[2];
+
+        } else if (line.indexOf("@@") === 0) {
+          /* Diff Block Header Line */
+
+          var values = /^(@@ -(\d+),(\d+) \+(\d+),(\d+) @@).*/.exec(line);
+
+          /* add previous block(if exists) before start a new one */
+          if (currentBlock) {
+            currentFile.blocks.push(currentBlock);
+            currentBlock = null;
+          }
+
+          /* create block metadata */
+          currentBlock = {};
+          currentBlock.lines = [];
+          currentBlock.oldStartLine = oldLine = values[2];
+          currentBlock.newStartLine = newLine = values[4];
+          currentBlock.deletedLines = values[3];
+          currentBlock.addedLines = values[5];
+
+          /* create block header line */
+          var currentLine = {};
+          currentLine.type = CSS_STYLES.INFO;
+          currentLine.content = line;
+          currentLine.oldNumber = BLOCK_HEADER_LINE;
+          currentLine.newNumber = BLOCK_HEADER_LINE;
+
+          /* add line to block */
+          currentBlock.lines.push(currentLine);
+
+        } else {
+          /* Regular Diff Line */
+
+          var currentLine = {};
+          currentLine.content = line;
+
+          if (line.indexOf("+") === 0) {
+            currentLine.type = CSS_STYLES.NEW;
+            currentLine.oldNumber = null;
+            currentLine.newNumber = newLine++;
+
+          } else if (line.indexOf("-") === 0) {
+            currentLine.type = CSS_STYLES.DELETED;
+            currentLine.oldNumber = oldLine++;
+            currentLine.newNumber = null;
+
+          } else {
+            currentLine.type = CSS_STYLES.CONTEXT;
+            currentLine.oldNumber = oldLine++;
+            currentLine.newNumber = newLine++;
+
+          }
+
+          /* add line to block */
+          currentBlock.lines.push(currentLine);
+        }
+      });
+
+      /* add previous block(if exists) before start a new file */
+      if (currentBlock) {
+        currentFile.blocks.push(currentBlock);
+        currentBlock = null;
+      }
+
+      /* add previous file(if exists) before start a new one */
+      if (currentFile) {
+        files.push(currentFile);
+        currentFile = null;
+      }
+
+      return files;
+    };
+
+    var generateHtml = function( diffFiles ) {
+      return diffFiles.map(function( file ) {
+        var fileHeader = file.oldName === file.newName ? file.newName : file.oldName + " -> " + file.newName
+
+        return "<div class=\"file-wrapper\">" +
+               "  <div class=\"file-header\">" +
+               "    <div class=\"file-name\">" + fileHeader + "</div>" +
+               "  </div>" +
+               "  <div class=\"file-diff\">" +
+               "    <div class=\"code-wrapper\">" +
+               "      <table class=\"diff-table\">" +
+               "        <tbody>" +
+                          generateFileHtml(file) +
+               "        </tbody>" +
+               "      </table>" +
+               "    </div>" +
+               "  </div>" +
+               "</div>";
+      });
+    };
+
+    var generateFileHtml = function(file) {
+      return file.blocks.map(function(block) {
+        return block.lines.map(function(line) {
+
+          var oldLine = line.oldNumber ? line.oldNumber : "",
+            newLine = line.newNumber ? line.newNumber : "";
+
+          return "<tr>" +
+            "  <td class=\"code-linenumber " + line.type + "\">" + oldLine + "</td>" +
+            "  <td class=\"code-linenumber " + line.type + "\">" + newLine + "</td>" +
+            "  <td class=\"code-line " + line.type + "\">" + escape(line.content) + "</td>" +
+            "</tr>";
+        }).join("\n");
+      }).join("\n");
+    };
+
+    var escape = function(str) {
+      return str
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/\t/g, "    ");
+    };
+
+    /* singleton pattern */
+    var instance;
+    return {
+      getInstance: function() {
+        if (instance === undefined) {
+          instance = new Diff2Html();
+          /* Hide the constructor so the returned objected can't be new'd */
+          instance.constructor = null;
+        }
+        return instance;
+      }
+    };
+
+  })();
+
+  window.Diff2Html = ClassVariable;
+  return window.Diff2Html;
+
+})(jQuery, window);
