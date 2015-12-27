97eafcd2f76856e8430d68c5e5bfd0f3ab0e50c4
Paulo Bu <pbu_98@yahoo.com>
2015-12-27 11:08:24 +0100

    Add tests to generateLineHtml for LineByLine tests

--- test/line-by-line-tests.js
+++ test/line-by-line-tests.js
@@ -19,4 +19,46 @@ describe('LineByLinePrinter', function() {
       assert.equal(expected, fileHtml);
     });
   });
+  describe('_generateLineHtml', function() {
+    it('should work for insertions', function() {
+
+      var diffParser = require('../src/diff-parser.js').DiffParser;
+      var lineByLinePrinter = new LineByLinePrinter({});
+      var fileHtml = lineByLinePrinter._generateLineHtml(
+        diffParser.LINE_TYPE.INSERTS, '', 30, '+', 'test');
+      var expected = '<tr>\n' +
+      '  <td class="d2h-code-linenumber d2h-ins">' +
+      '    <div class="line-num1"></div>' +
+      '    <div class="line-num2">30</div>' +
+      '  </td>\n' +
+      '  <td class="d2h-ins">' +
+      '    <div class="d2h-code-line d2h-ins">' +
+      '<span class="d2h-code-line-prefix">test</span>' +
+      '<span class="d2h-code-line-ctn">+</span></div>' +
+      '  </td>\n' +
+      '</tr>\n';
+
+      assert.equal(expected, fileHtml);
+    });
+    it('should work for deletions', function() {
+
+      var diffParser = require('../src/diff-parser.js').DiffParser;
+      var lineByLinePrinter = new LineByLinePrinter({});
+      var fileHtml = lineByLinePrinter._generateLineHtml(
+        diffParser.LINE_TYPE.DELETES, 30, '', '-', 'test');
+      var expected = '<tr>\n' +
+      '  <td class="d2h-code-linenumber d2h-del">' +
+      '    <div class="line-num1">30</div>' +
+      '    <div class="line-num2"></div>' +
+      '  </td>\n' +
+      '  <td class="d2h-del">' +
+      '    <div class="d2h-code-line d2h-del">' +
+      '<span class="d2h-code-line-prefix">test</span>' +
+      '<span class="d2h-code-line-ctn">-</span></div>' +
+      '  </td>\n' +
+      '</tr>\n';
+
+      assert.equal(expected, fileHtml);
+    });
+  });
 });
