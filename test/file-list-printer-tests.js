7fbc8c238af796292c0e92b001d66dfb8390f7bd
Rodrigo Fernandes <rtfrodrigo@gmail.com>
2018-11-18 15:38:46 +0000

    Improve line-height

--- test/file-list-printer-tests.js
+++ test/file-list-printer-tests.js
@@ -45,11 +45,10 @@ describe('FileListPrinter', function() {
         '    <ol class="d2h-file-list">\n' +
         '    <li class="d2h-file-list-line">\n' +
         '    <span class="d2h-file-name-wrapper">\n' +
-        '      <span><svg aria-hidden="true" class="d2h-icon d2h-changed" height="16" title="modified" version="1.1"\n' +
-        '     viewBox="0 0 14 16" width="14">\n' +
-        '    <path d="M13 1H1C0.45 1 0 1.45 0 2v12c0 0.55 0.45 1 1 1h12c0.55 0 1-0.45 1-1V2c0-0.55-0.45-1-1-1z m0 13H1V2h12v12zM4 8c0-1.66 1.34-3 3-3s3 1.34 3 3-1.34 3-3 3-3-1.34-3-3z"></path>\n' +
-        '</svg></span>\n' +
-        '      <a href="#d2h-781444" class="d2h-file-name">my/file/name.js</a>\n' +
+        '      <svg aria-hidden="true" class="d2h-icon d2h-changed" height="16" title="modified" version="1.1"\n' +
+        '           viewBox="0 0 14 16" width="14">\n' +
+        '          <path d="M13 1H1C0.45 1 0 1.45 0 2v12c0 0.55 0.45 1 1 1h12c0.55 0 1-0.45 1-1V2c0-0.55-0.45-1-1-1z m0 13H1V2h12v12zM4 8c0-1.66 1.34-3 3-3s3 1.34 3 3-1.34 3-3 3-3-1.34-3-3z"></path>\n' +
+        '      </svg>      <a href="#d2h-781444" class="d2h-file-name">my/file/name.js</a>\n' +
         '      <span class="d2h-file-stats">\n' +
         '          <span class="d2h-lines-added">+12</span>\n' +
         '          <span class="d2h-lines-deleted">-41</span>\n' +
@@ -58,11 +57,10 @@ describe('FileListPrinter', function() {
         '</li>\n' +
         '<li class="d2h-file-list-line">\n' +
         '    <span class="d2h-file-name-wrapper">\n' +
-        '      <span><svg aria-hidden="true" class="d2h-icon d2h-moved" height="16" title="renamed" version="1.1"\n' +
-        '     viewBox="0 0 14 16" width="14">\n' +
-        '    <path d="M6 9H3V7h3V4l5 4-5 4V9z m8-7v12c0 0.55-0.45 1-1 1H1c-0.55 0-1-0.45-1-1V2c0-0.55 0.45-1 1-1h12c0.55 0 1 0.45 1 1z m-1 0H1v12h12V2z"></path>\n' +
-        '</svg></span>\n' +
-        '      <a href="#d2h-662683" class="d2h-file-name">my/file/{name1.js → name2.js}</a>\n' +
+        '      <svg aria-hidden="true" class="d2h-icon d2h-moved" height="16" title="renamed" version="1.1"\n' +
+        '           viewBox="0 0 14 16" width="14">\n' +
+        '          <path d="M6 9H3V7h3V4l5 4-5 4V9z m8-7v12c0 0.55-0.45 1-1 1H1c-0.55 0-1-0.45-1-1V2c0-0.55 0.45-1 1-1h12c0.55 0 1 0.45 1 1z m-1 0H1v12h12V2z"></path>\n' +
+        '      </svg>      <a href="#d2h-662683" class="d2h-file-name">my/file/{name1.js → name2.js}</a>\n' +
         '      <span class="d2h-file-stats">\n' +
         '          <span class="d2h-lines-added">+12</span>\n' +
         '          <span class="d2h-lines-deleted">-41</span>\n' +
@@ -71,11 +69,10 @@ describe('FileListPrinter', function() {
         '</li>\n' +
         '<li class="d2h-file-list-line">\n' +
         '    <span class="d2h-file-name-wrapper">\n' +
-        '      <span><svg aria-hidden="true" class="d2h-icon d2h-added" height="16" title="added" version="1.1" viewBox="0 0 14 16"\n' +
-        '     width="14">\n' +
-        '    <path d="M13 1H1C0.45 1 0 1.45 0 2v12c0 0.55 0.45 1 1 1h12c0.55 0 1-0.45 1-1V2c0-0.55-0.45-1-1-1z m0 13H1V2h12v12zM6 9H3V7h3V4h2v3h3v2H8v3H6V9z"></path>\n' +
-        '</svg></span>\n' +
-        '      <a href="#d2h-781444" class="d2h-file-name">my/file/name.js</a>\n' +
+        '      <svg aria-hidden="true" class="d2h-icon d2h-added" height="16" title="added" version="1.1" viewBox="0 0 14 16"\n' +
+        '           width="14">\n' +
+        '          <path d="M13 1H1C0.45 1 0 1.45 0 2v12c0 0.55 0.45 1 1 1h12c0.55 0 1-0.45 1-1V2c0-0.55-0.45-1-1-1z m0 13H1V2h12v12zM6 9H3V7h3V4h2v3h3v2H8v3H6V9z"></path>\n' +
+        '      </svg>      <a href="#d2h-781444" class="d2h-file-name">my/file/name.js</a>\n' +
         '      <span class="d2h-file-stats">\n' +
         '          <span class="d2h-lines-added">+12</span>\n' +
         '          <span class="d2h-lines-deleted">-0</span>\n' +
@@ -84,11 +81,10 @@ describe('FileListPrinter', function() {
         '</li>\n' +
         '<li class="d2h-file-list-line">\n' +
         '    <span class="d2h-file-name-wrapper">\n' +
-        '      <span><svg aria-hidden="true" class="d2h-icon d2h-deleted" height="16" title="removed" version="1.1"\n' +
-        '     viewBox="0 0 14 16" width="14">\n' +
-        '    <path d="M13 1H1C0.45 1 0 1.45 0 2v12c0 0.55 0.45 1 1 1h12c0.55 0 1-0.45 1-1V2c0-0.55-0.45-1-1-1z m0 13H1V2h12v12zM11 9H3V7h8v2z"></path>\n' +
-        '</svg></span>\n' +
-        '      <a href="#d2h-781444" class="d2h-file-name">my/file/name.js</a>\n' +
+        '      <svg aria-hidden="true" class="d2h-icon d2h-deleted" height="16" title="removed" version="1.1"\n' +
+        '           viewBox="0 0 14 16" width="14">\n' +
+        '          <path d="M13 1H1C0.45 1 0 1.45 0 2v12c0 0.55 0.45 1 1 1h12c0.55 0 1-0.45 1-1V2c0-0.55-0.45-1-1-1z m0 13H1V2h12v12zM11 9H3V7h8v2z"></path>\n' +
+        '      </svg>      <a href="#d2h-781444" class="d2h-file-name">my/file/name.js</a>\n' +
         '      <span class="d2h-file-stats">\n' +
         '          <span class="d2h-lines-added">+0</span>\n' +
         '          <span class="d2h-lines-deleted">-41</span>\n' +
--- test/line-by-line-tests.js
+++ test/line-by-line-tests.js
@@ -152,10 +152,9 @@ describe('LineByLinePrinter', function() {
         '<div id="d2h-781444" class="d2h-file-wrapper" data-lang="js">\n' +
         '    <div class="d2h-file-header">\n' +
         '    <span class="d2h-file-name-wrapper">\n' +
-        '    <span class="d2h-icon-wrapper"><svg aria-hidden="true" class="d2h-icon" height="16" version="1.1" viewBox="0 0 12 16" width="12">\n' +
-        '    <path d="M6 5H2v-1h4v1zM2 8h7v-1H2v1z m0 2h7v-1H2v1z m0 2h7v-1H2v1z m10-7.5v9.5c0 0.55-0.45 1-1 1H1c-0.55 0-1-0.45-1-1V2c0-0.55 0.45-1 1-1h7.5l3.5 3.5z m-1 0.5L8 2H1v12h10V5z"></path>\n' +
-        '</svg></span>\n' +
-        '    <span class="d2h-file-name">my/file/name.js</span>\n' +
+        '    <svg aria-hidden="true" class="d2h-icon" height="16" version="1.1" viewBox="0 0 12 16" width="12">\n' +
+        '        <path d="M6 5H2v-1h4v1zM2 8h7v-1H2v1z m0 2h7v-1H2v1z m0 2h7v-1H2v1z m10-7.5v9.5c0 0.55-0.45 1-1 1H1c-0.55 0-1-0.45-1-1V2c0-0.55 0.45-1 1-1h7.5l3.5 3.5z m-1 0.5L8 2H1v12h10V5z"></path>\n' +
+        '    </svg>    <span class="d2h-file-name">my/file/name.js</span>\n' +
         '    <span class="d2h-tag d2h-changed d2h-changed-tag">CHANGED</span></span>\n' +
         '    </div>\n' +
         '    <div class="d2h-file-diff">\n' +
@@ -190,10 +189,9 @@ describe('LineByLinePrinter', function() {
         '<div id="d2h-781444" class="d2h-file-wrapper" data-lang="js">\n' +
         '    <div class="d2h-file-header">\n' +
         '    <span class="d2h-file-name-wrapper">\n' +
-        '    <span class="d2h-icon-wrapper"><svg aria-hidden="true" class="d2h-icon" height="16" version="1.1" viewBox="0 0 12 16" width="12">\n' +
-        '    <path d="M6 5H2v-1h4v1zM2 8h7v-1H2v1z m0 2h7v-1H2v1z m0 2h7v-1H2v1z m10-7.5v9.5c0 0.55-0.45 1-1 1H1c-0.55 0-1-0.45-1-1V2c0-0.55 0.45-1 1-1h7.5l3.5 3.5z m-1 0.5L8 2H1v12h10V5z"></path>\n' +
-        '</svg></span>\n' +
-        '    <span class="d2h-file-name">my/file/name.js</span>\n' +
+        '    <svg aria-hidden="true" class="d2h-icon" height="16" version="1.1" viewBox="0 0 12 16" width="12">\n' +
+        '        <path d="M6 5H2v-1h4v1zM2 8h7v-1H2v1z m0 2h7v-1H2v1z m0 2h7v-1H2v1z m10-7.5v9.5c0 0.55-0.45 1-1 1H1c-0.55 0-1-0.45-1-1V2c0-0.55 0.45-1 1-1h7.5l3.5 3.5z m-1 0.5L8 2H1v12h10V5z"></path>\n' +
+        '    </svg>    <span class="d2h-file-name">my/file/name.js</span>\n' +
         '    <span class="d2h-tag d2h-added d2h-added-tag">ADDED</span></span>\n' +
         '    </div>\n' +
         '    <div class="d2h-file-diff">\n' +
@@ -228,10 +226,9 @@ describe('LineByLinePrinter', function() {
         '<div id="d2h-781444" class="d2h-file-wrapper" data-lang="js">\n' +
         '    <div class="d2h-file-header">\n' +
         '    <span class="d2h-file-name-wrapper">\n' +
-        '    <span class="d2h-icon-wrapper"><svg aria-hidden="true" class="d2h-icon" height="16" version="1.1" viewBox="0 0 12 16" width="12">\n' +
-        '    <path d="M6 5H2v-1h4v1zM2 8h7v-1H2v1z m0 2h7v-1H2v1z m0 2h7v-1H2v1z m10-7.5v9.5c0 0.55-0.45 1-1 1H1c-0.55 0-1-0.45-1-1V2c0-0.55 0.45-1 1-1h7.5l3.5 3.5z m-1 0.5L8 2H1v12h10V5z"></path>\n' +
-        '</svg></span>\n' +
-        '    <span class="d2h-file-name">my/file/name.js</span>\n' +
+        '    <svg aria-hidden="true" class="d2h-icon" height="16" version="1.1" viewBox="0 0 12 16" width="12">\n' +
+        '        <path d="M6 5H2v-1h4v1zM2 8h7v-1H2v1z m0 2h7v-1H2v1z m0 2h7v-1H2v1z m10-7.5v9.5c0 0.55-0.45 1-1 1H1c-0.55 0-1-0.45-1-1V2c0-0.55 0.45-1 1-1h7.5l3.5 3.5z m-1 0.5L8 2H1v12h10V5z"></path>\n' +
+        '    </svg>    <span class="d2h-file-name">my/file/name.js</span>\n' +
         '    <span class="d2h-tag d2h-deleted d2h-deleted-tag">DELETED</span></span>\n' +
         '    </div>\n' +
         '    <div class="d2h-file-diff">\n' +
@@ -266,10 +263,9 @@ describe('LineByLinePrinter', function() {
         '<div id="d2h-662683" class="d2h-file-wrapper" data-lang="js">\n' +
         '    <div class="d2h-file-header">\n' +
         '    <span class="d2h-file-name-wrapper">\n' +
-        '    <span class="d2h-icon-wrapper"><svg aria-hidden="true" class="d2h-icon" height="16" version="1.1" viewBox="0 0 12 16" width="12">\n' +
-        '    <path d="M6 5H2v-1h4v1zM2 8h7v-1H2v1z m0 2h7v-1H2v1z m0 2h7v-1H2v1z m10-7.5v9.5c0 0.55-0.45 1-1 1H1c-0.55 0-1-0.45-1-1V2c0-0.55 0.45-1 1-1h7.5l3.5 3.5z m-1 0.5L8 2H1v12h10V5z"></path>\n' +
-        '</svg></span>\n' +
-        '    <span class="d2h-file-name">my/file/{name1.js → name2.js}</span>\n' +
+        '    <svg aria-hidden="true" class="d2h-icon" height="16" version="1.1" viewBox="0 0 12 16" width="12">\n' +
+        '        <path d="M6 5H2v-1h4v1zM2 8h7v-1H2v1z m0 2h7v-1H2v1z m0 2h7v-1H2v1z m10-7.5v9.5c0 0.55-0.45 1-1 1H1c-0.55 0-1-0.45-1-1V2c0-0.55 0.45-1 1-1h7.5l3.5 3.5z m-1 0.5L8 2H1v12h10V5z"></path>\n' +
+        '    </svg>    <span class="d2h-file-name">my/file/{name1.js → name2.js}</span>\n' +
         '    <span class="d2h-tag d2h-moved d2h-moved-tag">RENAMED</span></span>\n' +
         '    </div>\n' +
         '    <div class="d2h-file-diff">\n' +
@@ -345,10 +341,9 @@ describe('LineByLinePrinter', function() {
         '    <div id="d2h-675094" class="d2h-file-wrapper" data-lang="">\n' +
         '    <div class="d2h-file-header">\n' +
         '    <span class="d2h-file-name-wrapper">\n' +
-        '    <span class="d2h-icon-wrapper"><svg aria-hidden="true" class="d2h-icon" height="16" version="1.1" viewBox="0 0 12 16" width="12">\n' +
-        '    <path d="M6 5H2v-1h4v1zM2 8h7v-1H2v1z m0 2h7v-1H2v1z m0 2h7v-1H2v1z m10-7.5v9.5c0 0.55-0.45 1-1 1H1c-0.55 0-1-0.45-1-1V2c0-0.55 0.45-1 1-1h7.5l3.5 3.5z m-1 0.5L8 2H1v12h10V5z"></path>\n' +
-        '</svg></span>\n' +
-        '    <span class="d2h-file-name">sample</span>\n' +
+        '    <svg aria-hidden="true" class="d2h-icon" height="16" version="1.1" viewBox="0 0 12 16" width="12">\n' +
+        '        <path d="M6 5H2v-1h4v1zM2 8h7v-1H2v1z m0 2h7v-1H2v1z m0 2h7v-1H2v1z m10-7.5v9.5c0 0.55-0.45 1-1 1H1c-0.55 0-1-0.45-1-1V2c0-0.55 0.45-1 1-1h7.5l3.5 3.5z m-1 0.5L8 2H1v12h10V5z"></path>\n' +
+        '    </svg>    <span class="d2h-file-name">sample</span>\n' +
         '    <span class="d2h-tag d2h-changed d2h-changed-tag">CHANGED</span></span>\n' +
         '    </div>\n' +
         '    <div class="d2h-file-diff">\n' +
@@ -411,10 +406,9 @@ describe('LineByLinePrinter', function() {
         '    <div id="d2h-675094" class="d2h-file-wrapper" data-lang="js">\n' +
         '    <div class="d2h-file-header">\n' +
         '    <span class="d2h-file-name-wrapper">\n' +
-        '    <span class="d2h-icon-wrapper"><svg aria-hidden="true" class="d2h-icon" height="16" version="1.1" viewBox="0 0 12 16" width="12">\n' +
-        '    <path d="M6 5H2v-1h4v1zM2 8h7v-1H2v1z m0 2h7v-1H2v1z m0 2h7v-1H2v1z m10-7.5v9.5c0 0.55-0.45 1-1 1H1c-0.55 0-1-0.45-1-1V2c0-0.55 0.45-1 1-1h7.5l3.5 3.5z m-1 0.5L8 2H1v12h10V5z"></path>\n' +
-        '</svg></span>\n' +
-        '    <span class="d2h-file-name">sample</span>\n' +
+        '    <svg aria-hidden="true" class="d2h-icon" height="16" version="1.1" viewBox="0 0 12 16" width="12">\n' +
+        '        <path d="M6 5H2v-1h4v1zM2 8h7v-1H2v1z m0 2h7v-1H2v1z m0 2h7v-1H2v1z m10-7.5v9.5c0 0.55-0.45 1-1 1H1c-0.55 0-1-0.45-1-1V2c0-0.55 0.45-1 1-1h7.5l3.5 3.5z m-1 0.5L8 2H1v12h10V5z"></path>\n' +
+        '    </svg>    <span class="d2h-file-name">sample</span>\n' +
         '    <span class="d2h-tag d2h-changed d2h-changed-tag">CHANGED</span></span>\n' +
         '    </div>\n' +
         '    <div class="d2h-file-diff">\n' +
